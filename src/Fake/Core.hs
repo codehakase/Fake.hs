a00670a195c1e39de03d5bfd96de2d4351e13d5d
Francis Sunday <codehakase@gmail.com>
2025-12-09 20:00:00 +0100

    init project

---
+++ src/Fake/Core.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Fake.Core
+  ( Fake
+  , runFaker
+  , runFakerSeed
+  , runFakerWith
+  , liftState
+  ) where
+
+import Control.Monad.State (State, evalState)
+import System.Random (StdGen, mkStdGen, initStdGen)
+
+newtype Fake a = Fake (State StdGen a)
+  deriving (Functor, Applicative, Monad)
+
+runFaker :: Fake a -> IO a
+runFaker (Fake computation) = do
+  gen <- initStdGen
+  return $ evalState computation gen
+
+runFakerSeed :: Int -> Fake a -> a
+runFakerSeed seed (Fake computation) =
+  let gen = mkStdGen seed
+  in evalState computation gen
+
+runFakerWith :: StdGen -> Fake a -> a
+runFakerWith gen (Fake computation) = evalState computation gen
+
+liftState :: State StdGen a -> Fake a
+liftState st = Fake st
